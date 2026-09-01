9293fa4db36ab4e3365b4ff9cdf39d21f47eea18
Sanlixx <santiagopre777@gmail.com>
2026-09-01 20:01:15 -0300

    Cambio

--- src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
+++ src/ar/edu/unlu/poo/labo1/artefactos/CatalogoArtefactos.java
@@ -1,4 +1,37 @@
 package ar.edu.unlu.poo.labo1.artefactos;
 
+import ar.edu.unlu.poo.labo1.libreria.Libro;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CatalogoArtefactos {
+    public CatalogoArtefactos() {
+        List <Artefacto> misArtefactos = new ArrayList<>();
+    }
+    public void agregarArtefacto(Artefacto artefacto){
+        for (Artefacto miArtefacto: misArtefactos){
+            if (miArtefacto.equals(artefacto)){
+
+            }
+        }
+        if (artefacto.equals)
+        misArtefactos.add(Artefacto artefacto);
+    }
+
+public Set obtenerArtefactosUnicos(){
+
 }
+
+
+public List buscarArtefactosPorTipo(String tipo){
+    List <Artefacto> tiposDeArtefactos = new ArrayList<>();
+}
+public Map<String, Integer> contarArtefactosPorTipo(){
+
+}
+public Artefacto obtenerArtefactoMasPoderoso(){
+
+}
+    }
